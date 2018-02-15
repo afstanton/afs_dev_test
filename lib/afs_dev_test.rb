87e9133f1e80868de09edd936d39389195d8c844
Aaron F Stanton <afstanton@gmail.com>
2018-02-15 09:14:07 -0500

    maybe that matters

--- lib/afs_dev_test.rb
+++ lib/afs_dev_test.rb
@@ -1,6 +1,5 @@
 require "afs_dev_test/version"
-require "afs_dev_test/railtie" if defined?(Rails)
 
 module AfsDevTest
-  # Your code goes here...
+  require "afs_dev_test/railtie" if defined?(Rails)
 end
